347150fb430b95011e8c5f0c73662cfb584f7eaf
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-08-08 20:27:36 +0800

    多语言基础架构

---
+++ AdmiralRoom/Common/ResourceService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huoyaoyuan.AdmiralRoom
+{
+    class ResourceService : NotifyBase
+    {
+        public static ResourceService Current { get; } = new ResourceService();
+        public static IReadOnlyCollection<CultureInfo> SupportedCultures { get; }=
+            new[] {"zh-CN","ja","en" }
+            .Select(x => {
+                try { return CultureInfo.GetCultureInfo(x); }
+                catch { return null; }
+            })
+            .Where(x => x != null)
+			.ToList();
+
+        public static Properties.Resources Resources { get; } = new Properties.Resources();
+        public void ChangeCulture(string CultureName)
+        {
+            CultureInfo culture = SupportedCultures.SingleOrDefault(x => x.Name == CultureName);
+            if (culture != null) Properties.Resources.Culture = culture;
+            OnPropertyChanged();
+        }
+    }
+}
--- AdmiralRoom/Properties/Resources.Designer.cs
+++ AdmiralRoom/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Huoyaoyuan.AdmiralRoom.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Huoyaoyuan.AdmiralRoom.Properties {
         ///   返回此类使用的缓存的 ResourceManager 实例。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Huoyaoyuan.AdmiralRoom.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace Huoyaoyuan.AdmiralRoom.Properties {
         ///   重写当前线程的 CurrentUICulture 属性。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
