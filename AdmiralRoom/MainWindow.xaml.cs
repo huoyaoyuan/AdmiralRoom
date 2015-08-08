b4373f2fbbe322f1cefacff9fe8b797e1f740556
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-08-08 18:00:26 +0800

    程序集设置

--- AdmiralRoom/MainWindow.xaml.cs
+++ AdmiralRoom/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AdmiralRoom
+namespace Huoyaoyuan.AdmiralRoom
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
