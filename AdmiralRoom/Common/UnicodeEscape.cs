b9554dad7052d03397d36cc9e704d3f384ac6aac
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-09-10 20:05:01 +0800

    试验UI

---
+++ AdmiralRoom/Common/UnicodeEscape.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Huoyaoyuan.AdmiralRoom
+{
+    class UnicodeEscape
+    {
+        static Regex reUnicode = new Regex(@"\\u([0-9a-fA-F]{4})", RegexOptions.Compiled);
+        public static string Decode(string s)
+        {
+            return reUnicode.Replace(s, m =>
+            {
+                short c;
+                if (short.TryParse(m.Groups[1].Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out c))
+                {
+                    return "" + (char)c;
+                }
+                return m.Value;
+            });
+        }
+    }
+}
---
+++ AdmiralRoom/Officer/Converter/TransparencyConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Media;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Huoyaoyuan.AdmiralRoom.Officer.Converter
+{
+    class TransparencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as SolidColorBrush;
+            var para = double.Parse(parameter.ToString());
+            var c = v.Color;
+            c.A = (byte)(c.A * para);
+            return new SolidColorBrush(c);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as SolidColorBrush;
+            var para = double.Parse(parameter.ToString());
+            var c = v.Color;
+            c.A = (byte)(c.A / para);
+            return new SolidColorBrush(c);
+        }
+    }
+}
