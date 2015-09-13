d18426275aedca4ec42d5be04770f400288e0937
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-09-13 16:24:15 +0800

    APIView功能；初步布局架构

--- AdmiralRoom/MainWindow.xaml.cs
+++ AdmiralRoom/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Xceed.Wpf.AvalonDock.Layout;
 using Xceed.Wpf.AvalonDock.Layout.Serialization;
+using Huoyaoyuan.AdmiralRoom.Views;
 
 namespace Huoyaoyuan.AdmiralRoom
 {
@@ -77,6 +78,22 @@ namespace Huoyaoyuan.AdmiralRoom
 
             this.Loaded += (_, __) => GameHost.Browser.Navigate(Properties.Settings.Default.GameUrl);
         }
+        
+        private void MakeViewList(ILayoutElement elem)
+        {
+            if(elem is LayoutAnchorable)
+            {
+                ViewList.Add((elem as LayoutAnchorable).ContentId, elem as LayoutAnchorable);
+                return;
+            }
+            if (elem is ILayoutContainer)
+            {
+                foreach (var child in (elem as ILayoutContainer).Children)
+                {
+                    MakeViewList(child);
+                }
+            }
+        }
 
         private void LoadLayout(object sender, RoutedEventArgs e)
         {
@@ -87,11 +104,51 @@ namespace Huoyaoyuan.AdmiralRoom
                 s.Deserialize("layout.xml");
             }
             catch { }
+            MakeViewList(DockMan.Layout);
         }
         private void SaveLayout(object sender, RoutedEventArgs e)
         {
             var s = new XmlLayoutSerializer(DockMan);
             s.Serialize("layout.xml");
+        }
+
+        private Dictionary<string, Type> ViewTypeList = new Dictionary<string, Type>()
+        {
+            ["APIView"] = typeof(APIView),
+        };
+        private Dictionary<string, LayoutAnchorable> ViewList = new Dictionary<string, LayoutAnchorable>();
+        private void SetToggleBinding(object sender, RoutedEventArgs e)
+        {
+            Binding ToggleBinding = new Binding();
+            string ViewName = (sender as Control).Tag as string;
+            //var TargetView = FindView(DockMan.Layout, ViewName);
+            LayoutAnchorable TargetView;
+            if (!ViewList.TryGetValue(ViewName, out TargetView))
+            {
+                Type ViewType;
+                if(!ViewTypeList.TryGetValue((sender as Control).Tag as string, out ViewType))
+                {
+                    System.Diagnostics.Debug.WriteLine($"Invalid View name: {ViewName}");
+                    return;
+                }
+                if (!ViewType.IsSubclassOf(typeof(Control)))
+                {
+                    System.Diagnostics.Debug.WriteLine($"Invalid View type: {ViewName}");
+                    return;
+                }
+                TargetView = new LayoutAnchorable();
+                TargetView.Content = ViewType.GetConstructor(new Type[0]).Invoke(new object[0]);
+                TargetView.ContentId = ViewName;
+                TargetView.Title = ViewName;
+#warning 从资源里读取View Title!
+                ViewList.Add(ViewName, TargetView);
+                TargetView.AddToLayout(DockMan, AnchorableShowStrategy.Right);
+                TargetView.Hide();
+            }
+            ToggleBinding.Source = TargetView;
+            ToggleBinding.Path = new PropertyPath("IsVisible");
+            ToggleBinding.Mode = BindingMode.TwoWay;
+            (sender as Fluent.ToggleButton).SetBinding(Fluent.ToggleButton.IsCheckedProperty, ToggleBinding);
         }
     }
 }
---
+++ AdmiralRoom/Models/APIModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huoyaoyuan.AdmiralRoom.Models
+{
+    class APIModel :NotifyBase
+    {
+        public static APIModel Current { get; } = new APIModel();
+        #region APIText
+        private string _apitext = "";
+        public string APIText
+        {
+            get { return _apitext; }
+            set
+            {
+                if (_apitext != value)
+                {
+                    _apitext = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        #endregion
+    }
+}
