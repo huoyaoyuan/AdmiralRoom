578c6803ab60dcac4302f526f3039f8a9deb08f3
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-10-19 15:52:50 +0800

    Dispose MemoryStream

--- AdmiralRoom/API/APIHelper.cs
+++ AdmiralRoom/API/APIHelper.cs
@@ -11,40 +11,44 @@ namespace Huoyaoyuan.AdmiralRoom
         public static APIData Parse(this Session oSession)
         {
             var resarr = oSession.ResponseBody;
-            var mms = new MemoryStream(resarr, 7, resarr.Length - 7, false);
             var serializer = svdata.Serializer;
             svdata _svdata = null;
-            try
+            using (var mms = new MemoryStream(resarr, 7, resarr.Length - 7, false))
             {
-                _svdata = serializer.ReadObject(mms) as svdata;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                var apistring = (new System.Text.UTF8Encoding()).GetString(mms.ToArray());
-                Debug.WriteLine(apistring);
-                Debugger.Break();
-                _svdata = new svdata();
+                try
+                {
+                    _svdata = serializer.ReadObject(mms) as svdata;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    var apistring = (new System.Text.UTF8Encoding()).GetString(mms.ToArray());
+                    Debug.WriteLine(apistring);
+                    Debugger.Break();
+                    _svdata = new svdata();
+                }
             }
             return new APIData(_svdata, oSession.GetRequestBodyAsString());
         }
         public static APIData<T> Parse<T>(this Session oSession)
         {
             var resarr = oSession.ResponseBody;
-            var mms = new MemoryStream(resarr, 7, resarr.Length - 7, false);
             var serializer = svdata<T>.Serializer;
             svdata<T> svdata = null;
-            try
+            using (var mms = new MemoryStream(resarr, 7, resarr.Length - 7, false))
             {
-                svdata = serializer.ReadObject(mms) as svdata<T>;
-            }
-            catch(Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-                var apistring = (new System.Text.UTF8Encoding()).GetString(mms.ToArray());
-                Debug.WriteLine(apistring);
-                Debugger.Break();
-                svdata = new svdata<T>();
+                try
+                {
+                    svdata = serializer.ReadObject(mms) as svdata<T>;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    var apistring = (new System.Text.UTF8Encoding()).GetString(mms.ToArray());
+                    Debug.WriteLine(apistring);
+                    Debugger.Break();
+                    svdata = new svdata<T>();
+                }
             }
             return new APIData<T>(svdata, oSession.GetRequestBodyAsString());
         }
