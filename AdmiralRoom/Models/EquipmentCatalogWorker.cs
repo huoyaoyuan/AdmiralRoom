af1f1d56e43a21ff12e65b5b25c74f233d0cead1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-12-15 17:09:22 +0800

    全装备列表：改修/熟练度显示

--- AdmiralRoom/Models/EquipmentCatalogWorker.cs
+++ AdmiralRoom/Models/EquipmentCatalogWorker.cs
@@ -9,9 +9,15 @@ namespace Huoyaoyuan.AdmiralRoom.Models
         public class EquipmentGroup
         {
             public EquipInfo Item { get; set; }
+            public List<EquipmentImprovementGroup> Groups { get; } = new List<EquipmentImprovementGroup>();
+        }
+        public class EquipmentImprovementGroup
+        {
+            public int Level { get; set; }
+            public int ImprovementType { get; set; }
             public int Count { get; set; }
             public int Left { get; set; }
-            public Dictionary<Ship, int> Equipped { get; set; }
+            public Dictionary<Ship, int> Equipped { get; } = new Dictionary<Ship, int>();
         }
         private EquipmentCatalogWorker() { }
         public static EquipmentCatalogWorker Instance { get; } = new EquipmentCatalogWorker();
@@ -42,16 +48,31 @@ namespace Huoyaoyuan.AdmiralRoom.Models
                 EquipmentGroup group;
                 if (!d.TryGetValue(typeid, out group))
                 {
-                    group = new EquipmentGroup { Item = item.EquipInfo, Equipped = new Dictionary<Ship, int>() };
+                    group = new EquipmentGroup { Item = item.EquipInfo };
                     d.Add(typeid, group);
                 }
-                group.Count++;
+                EquipmentImprovementGroup group2 = null;
+                int i;
+                for (i = 0; i < group.Groups.Count; i++)
+                    if (group.Groups[i].Level >= item.AirProficiency + item.ImproveLevel)
+                    {
+                        if (group.Groups[i].Level == item.AirProficiency + item.ImproveLevel) group2 = group.Groups[i];
+                        break;
+                    }
+                if (group2 == null)
+                {
+                    group2 = new EquipmentImprovementGroup { Level = item.AirProficiency + item.ImproveLevel };
+                    if (item.ImproveLevel > 0) group2.ImprovementType = 1;
+                    else if (item.AirProficiency > 0) group2.ImprovementType = 2;
+                    group.Groups.Insert(i, group2);
+                }
+                group2.Count++;
                 if (item.OnShip != null)
                 {
-                    if (group.Equipped.ContainsKey(item.OnShip)) group.Equipped[item.OnShip]++;
-                    else group.Equipped.Add(item.OnShip, 1);
+                    if (group2.Equipped.ContainsKey(item.OnShip)) group2.Equipped[item.OnShip]++;
+                    else group2.Equipped.Add(item.OnShip, 1);
                 }
-                else group.Left++;
+                else group2.Left++;
             }
             Groups = d.Values.OrderBy(x => x.Item.EquipType.Id).ThenBy(x => x.Item.Id).ToArray();
         }
