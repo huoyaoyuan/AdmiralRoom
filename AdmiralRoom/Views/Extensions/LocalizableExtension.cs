05b231b8e772be6c87c1d7ff0ffe789e93a7aaa6
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-12-24 04:45:07 +0800

    LocalizableExtension

---
+++ AdmiralRoom/Views/Extensions/LocalizableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Huoyaoyuan.AdmiralRoom.Views.Extensions
+{
+    class LocalizableExtension : Binding
+    {
+        private string _resourcekey;
+        [ConstructorArgument("resourcekey")]
+        public string ResourceKey
+        {
+            get { return _resourcekey; }
+            set
+            {
+                _resourcekey = value;
+                Path = new PropertyPath("Resources." + value);
+            }
+        }
+        public LocalizableExtension(string resourcekey)
+        {
+            ResourceKey = resourcekey;
+            Source = ResourceService.Current;
+            Mode = BindingMode.OneWay;
+        }
+    }
+}
