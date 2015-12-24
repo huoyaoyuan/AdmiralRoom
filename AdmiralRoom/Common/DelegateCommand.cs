e929a89a0459f54488a039632ff164bb83a0e1b1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-12-25 04:00:18 +0800

    DelegateCommand

---
+++ AdmiralRoom/Common/DelegateCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+#pragma warning disable CS0067
+
+namespace Huoyaoyuan.AdmiralRoom
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action action;
+        public DelegateCommand(Action action)
+        {
+            this.action = action;
+        }
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => action();
+    }
+}
