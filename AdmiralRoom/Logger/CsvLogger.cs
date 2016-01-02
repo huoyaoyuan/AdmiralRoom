1ebe0954c4992dd1b50cf3ae1d3ab20542074985
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-01-02 22:00:39 +0800

    Logger基础

---
+++ AdmiralRoom/Logger/CsvLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+#pragma warning disable CC0022
+
+namespace Huoyaoyuan.AdmiralRoom.Logger
+{
+    public abstract class CsvLogger : Logger
+    {
+        private readonly FileInfo file;
+        protected CsvLogger(string filename)
+        {
+            if (Path.GetExtension(filename) != ".csv") throw new ArgumentException();
+            file = new FileInfo(filename);
+            if (!file.Exists)
+            {
+#pragma warning disable RECS0021
+                using (var sw = file.CreateText())
+                    Initialize(sw);
+#pragma warning restore RECS0021
+            }
+        }
+        protected abstract void Initialize(TextWriter writer);
+        protected sealed override void Log(params object[] data)
+        {
+            using (var fs = file.Open(FileMode.Append, FileAccess.Write))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                Log(sw, data);
+            }
+        }
+        protected void Log(TextWriter writer, params object[] data)
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                writer.Write(data[i]);
+                writer.Write(',');
+            }
+            writer.WriteLine(data[data.Length - 1]);
+        }
+    }
+}
---
+++ AdmiralRoom/Logger/Logger.cs
@@ -0,0 +1,8 @@
+﻿namespace Huoyaoyuan.AdmiralRoom.Logger
+{
+    public abstract class Logger
+    {
+        protected Logger() { }
+        protected abstract void Log(params object[] data);
+    }
+}
