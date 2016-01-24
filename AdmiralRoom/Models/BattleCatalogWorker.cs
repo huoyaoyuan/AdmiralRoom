6cb796bed6a1c539f3801388ad05c857cf73d608
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-01-23 22:42:28 -0500

    出击统计(壳)

---
+++ AdmiralRoom/Models/BattleCatalogWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huoyaoyuan.AdmiralRoom.Models
+{
+    public class BattleCatalogWorker
+    {
+        public class BattleCatalogViewModel
+        {
+            public string Title { get; set; }
+            public string Total { get; set; }
+            public string Win { get; set; }
+            public string S { get; set; }
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+            public string D { get; set; }
+            public BattleCatalogViewModel[] Items { get; set; }
+        }
+        public BattleCatalogViewModel[] Items { get; }
+    }
+}
