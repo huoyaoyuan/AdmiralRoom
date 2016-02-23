95899d2be4e80ea2ae2b14ede611b788cf752d00
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-02-23 17:36:09 -0500

    MEF接口

---
+++ AdmiralRoom/Composition/ModuleHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+
+namespace Huoyaoyuan.AdmiralRoom.Composition
+{
+    internal class ModuleHost
+    {
+        private ModuleHost() { }
+        public static ModuleHost Instance { get; }
+        [ImportMany(typeof(IModule))]
+        public IList<Lazy<IModule, IDictionary<string, object>>> Modules { get; set; }
+        public void Initialize()
+        {
+            using (var catalog = new AggregateCatalog(
+                new AssemblyCatalog(Assembly.GetExecutingAssembly()),
+                new DirectoryCatalog("modules")))
+            using (var container = new CompositionContainer(catalog))
+            {
+                container.ComposeParts(this);
+            }
+        }
+    }
+}
