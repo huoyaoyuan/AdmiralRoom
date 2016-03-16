892ee1b9ccc3752e446399925f4281df1466d110
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-03-15 22:10:43 -0400

    改修工厂：项目创建

---
+++ Modules/Akashi/Akashi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Huoyaoyuan.AdmiralRoom.Composition;
+
+namespace Huoyaoyuan.AdmiralRoom.Modules.Akashi
+{
+    public class Akashi : IModule
+    {
+        public IEnumerable<IChildView> ChildViews
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<IChildWindow> ChildWindows
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public FrameworkElement SettingView
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void OnCultureChanged(CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
