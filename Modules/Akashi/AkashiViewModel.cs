71c98802579d84959a86a820882052b5dff4644e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-03-17 20:46:17 -0400

    减少录入用属性

--- Modules/Akashi/AkashiViewModel.cs
+++ Modules/Akashi/AkashiViewModel.cs
@@ -35,10 +35,21 @@ namespace Huoyaoyuan.AdmiralRoom.Modules.Akashi
         public EquipInfo Equipment => Staff.Current.MasterData.EquipInfo?[EquipId];
         public List<SecretaryInfo> Secretaries { get; set; } = new List<SecretaryInfo>();
         public List<RangeInfo> Ranges { get; set; } = new List<RangeInfo>();
-        public int CostItem1 { get; set; }
-        public int CostItem2 { get; set; }
-        public int CostItem3 { get; set; }
-        public int CostItem4 { get; set; }
+        public int CostItem1 { get; private set; }
+        public int CostItem2 { get; private set; }
+        public int CostItem3 { get; private set; }
+        public int CostItem4 { get; private set; }
+        public string Cost
+        {
+            set
+            {
+                var costs = value.Split(',');
+                CostItem1 = int.Parse(costs[0]);
+                CostItem2 = int.Parse(costs[1]);
+                CostItem3 = int.Parse(costs[2]);
+                CostItem4 = int.Parse(costs[3]);
+            }
+        }
         public EnhancementInfo CopyOfWeekDay(int weekday) => new EnhancementInfo
         {
             EquipId = EquipId,
@@ -59,13 +70,33 @@ namespace Huoyaoyuan.AdmiralRoom.Modules.Akashi
     struct RangeInfo
     {
         public int Range { get; set; }
-        public int CostDevelopment { get; set; }
-        public int CostImprovement { get; set; }
-        public int CostDevelopmentConfirmed { get; set; }
-        public int CostImprovementConfirmed { get; set; }
-        public int CostItemId { get; set; }
+        public int CostDevelopment { get; private set; }
+        public int CostImprovement { get; private set; }
+        public int CostDevelopmentConfirmed { get; private set; }
+        public int CostImprovementConfirmed { get; private set; }
+        public string Cost
+        {
+            set
+            {
+                var costs = value.Split(',');
+                CostDevelopment = int.Parse(costs[0]);
+                CostDevelopmentConfirmed = int.Parse(costs[1]);
+                CostImprovement = int.Parse(costs[2]);
+                CostImprovementConfirmed = int.Parse(costs[3]);
+            }
+        }
+        public int CostItemId { get; private set; }
         public EquipInfo CostItem => Staff.Current.MasterData.EquipInfo?[CostItemId];
-        public int CostItemCount { get; set; }
+        public int CostItemCount { get; private set; }
+        public string CostEquip
+        {
+            set
+            {
+                var cost = value.Split(',');
+                CostItemId = int.Parse(cost[0]);
+                CostItemCount = int.Parse(cost[1]);
+            }
+        }
         public int UpgradeToId { get; set; }
         public EquipInfo UpgradeTo => Staff.Current.MasterData.EquipInfo?[UpgradeToId];
     }
