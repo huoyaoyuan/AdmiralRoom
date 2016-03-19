77bc8a75e99c194c9fe86cfaf9f911650d984a32
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-03-18 20:02:59 -0400

    模块信息

---
+++ Modules/MasterDataViewer/MasterDataViewer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using Huoyaoyuan.AdmiralRoom.Composition;
+
+namespace Huoyaoyuan.AdmiralRoom.Modules.MasterDataViewer
+{
+    [Export(typeof(IModule))]
+    public class MasterDataViewer : IModule
+    {
+        private class ChildWindow : IChildWindow
+        {
+            public ImageSource Icon => null;
+
+            public string Title => "MasterData";
+
+            public Type WindowType => typeof(MasterDataWindow);
+        }
+        public IEnumerable<IChildView> ChildViews => null;
+
+        public IEnumerable<IChildWindow> ChildWindows => new[] { new ChildWindow() };
+
+        public string Name => "MasterDataViewer";
+
+        public FrameworkElement SettingView => null;
+
+        public void OnCultureChanged(CultureInfo culture)
+        {
+            //nothing happens
+        }
+    }
+}
