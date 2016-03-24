22d02fbaa30d43d814aad26e9a1d50b1ddc668d8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-03-24 14:56:28 -0400

    Aero2

--- AdmiralRoom/Common/ThemeService.cs
+++ AdmiralRoom/Common/ThemeService.cs
@@ -87,19 +87,11 @@ namespace Huoyaoyuan.AdmiralRoom
             else
             {
                 Application.Current.Resources.MergedDictionaries.Clear();
-                if (ToastNotifier.IsSupported)//Win8OrGreater
+                Application.Current.Resources.MergedDictionaries.Add(App.LoadComponent(new Uri("PresentationFramework.Aero2, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35;component/themes/aero2.normalcolor.xaml", UriKind.Relative)) as ResourceDictionary);
+                Application.Current.MainWindow.SetTheme("Aero", new[]
                 {
-                    Application.Current.MainWindow.SetTheme("Aero", new string[] { });
-                    ChangeRibbonTheme(Config.Current.Theme, Application.Current.MainWindow as RibbonWindow);
-                }
-                else
-                {
-                    Application.Current.Resources.MergedDictionaries.Add(App.LoadComponent(new Uri("PresentationFramework.AeroLite, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35;component/themes/aerolite.normalcolor.xaml", UriKind.Relative)) as ResourceDictionary);
-                    Application.Current.MainWindow.SetTheme("Aero", new[]
-                    {
-                        "PresentationFramework.AeroLite, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35;component/themes/aerolite.normalcolor.xaml"
-                    });
-                }
+                    "PresentationFramework.Aero2, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35;component/themes/aero2.normalcolor.xaml"
+                });
             }
             Application.Current.MainWindow.SetThemePrior("Aero");
         }
