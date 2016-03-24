253e715b1cd77cb966871af384be61141025ef0a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-03-24 17:19:57 -0400

    v0.4.3

--- AdmiralRoom/Properties/AssemblyInfo.cs
+++ AdmiralRoom/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.2.490")]
-[assembly: AssemblyFileVersion("0.4.2.490")]
+[assembly: AssemblyVersion("0.4.3.504")]
+[assembly: AssemblyFileVersion("0.4.3.504")]
 [assembly: NeutralResourcesLanguage("en")]
 
