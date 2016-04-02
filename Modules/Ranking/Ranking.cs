dba4375e679a1e8f41ab9a2cf61ba998ea0e6619
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-04-02 15:21:34 -0400

    模块信息

---
+++ Modules/Ranking/Ranking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Globalization;
+using System.Windows;
+using Huoyaoyuan.AdmiralRoom.Composition;
+
+namespace Huoyaoyuan.AdmiralRoom.Modules.Ranking
+{
+    [Export(typeof(IModule))]
+    public class Ranking : IModule
+    {
+        private class ChildView : IChildView
+        {
+            public string ContentId => "RankingView";
+
+            public string Title => "戦果";
+
+            public FrameworkElement View { get; } = new RankingView();
+        }
+        public IEnumerable<IChildView> ChildViews => new[] { new ChildView() };
+
+        public IEnumerable<IChildWindow> ChildWindows => null;
+
+        public string Name => "戦果情報";
+
+        public FrameworkElement SettingView => null;
+
+        public void OnCultureChanged(CultureInfo culture)
+        {
+            //nothing happens
+        }
+    }
+}
