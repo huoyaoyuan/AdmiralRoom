62b8bad65dfdc9815981c0d180c4b54e88a6d9c8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-04 19:43:11 +0800

    创建NewWindow

---
+++ AdmiralRoom/NewWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Huoyaoyuan.AdmiralRoom
+{
+    /// <summary>
+    /// NewWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
