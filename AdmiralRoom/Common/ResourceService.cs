a85e1350122eb6f05e74586626a2e3459ee8d0af
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-10 01:14:02 +0800

    更改CultureChanged处理方式

--- AdmiralRoom/Common/ResourceService.cs
+++ AdmiralRoom/Common/ResourceService.cs
@@ -11,23 +11,29 @@ namespace Huoyaoyuan.AdmiralRoom
         public static ResourceService Current { get; } = new ResourceService();
         public static IReadOnlyCollection<CultureInfo> SupportedCultures { get; } =
             new[] { "zh-Hans", "ja", "en" }
-#pragma warning disable CC0020 // You should remove the lambda expression when it only invokes a method with the same signature
-            .Select(x =>
-            {
-                try { return CultureInfo.GetCultureInfo(x); }
-                catch { return null; }
-            })
-#pragma warning restore CC0020 // You should remove the lambda expression when it only invokes a method with the same signature
+            .Select(CultureInfo.GetCultureInfo)
             .Where(x => x != null)
             .ToList();
-
+        private CultureInfo _currentCulture;
+        public CultureInfo CurrentCulture
+        {
+            get { return _currentCulture; }
+            set
+            {
+                _currentCulture = value;
+                OnAllPropertyChanged();
+                CultureChanged?.Invoke(value);
+            }
+        }
         public Properties.Resources Resources { get; } = new Properties.Resources();
         public void ChangeCulture(string CultureName)
         {
             CultureInfo culture = SupportedCultures.SingleOrDefault(x => x.Name == CultureName);
-            if (culture != null) Properties.Resources.Culture = culture;
-            OnPropertyChanged(nameof(Resources));
-            CultureChanged?.Invoke(culture);
+            if (culture != null)
+            {
+                Properties.Resources.Culture = culture;
+                CurrentCulture = culture;
+            }
         }
         public event Action<CultureInfo> CultureChanged;
     }
