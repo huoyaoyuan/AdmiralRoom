d5ad8e5410e77d86c5db25f119328ec11d311b65
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-11 04:31:14 +0800

    StringTableConverter

---
+++ AdmiralRoom/Views/Converter/StringTableConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Huoyaoyuan.AdmiralRoom.Views.Converter
+{
+    public sealed class StringTableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ResourceService.GetString(parameter.ToString());
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
