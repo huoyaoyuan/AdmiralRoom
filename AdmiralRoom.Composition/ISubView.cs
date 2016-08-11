34667ac8766acb783fe2aa4e6f73def0af75b9a4
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-12 05:15:05 +0800

    xml注释

--- AdmiralRoom.Composition/ISubView.cs
+++ AdmiralRoom.Composition/ISubView.cs
@@ -3,10 +3,24 @@ using System.Windows;
 
 namespace Huoyaoyuan.AdmiralRoom.Composition
 {
+    /// <summary>
+    /// Associate with a subview that can be docked in the main window.
+    /// </summary>
     public interface ISubView
     {
+        /// <summary>
+        /// The sub view. 
+        /// </summary>
         UIElement View { get; }
+        /// <summary>
+        /// ContentID used by DockingManager to save the layout. Must be unique.
+        /// </summary>
         string ContentID { get; }
+        /// <summary>
+        /// Get localized title.
+        /// </summary>
+        /// <param name="culture">Expected language.</param>
+        /// <returns>Localized title.</returns>
         string GetTitle(CultureInfo culture);
     }
 }
--- AdmiralRoom.Composition/ISubWindow.cs
+++ AdmiralRoom.Composition/ISubWindow.cs
@@ -3,11 +3,43 @@ using System.Windows;
 
 namespace Huoyaoyuan.AdmiralRoom.Composition
 {
+    /// <summary>
+    /// Associate with a standalone window that has single instance at one time.
+    /// </summary>
     public interface ISubWindow
     {
+        /// <summary>
+        /// Create a new window. Must create a new one since the old one will be unavailable after closed.
+        /// </summary>
+        /// <returns></returns>
         Window CreateWindow();
+        /// <summary>
+        /// Get localized title.
+        /// </summary>
+        /// <param name="culture">Expected language.</param>
+        /// <returns>Localized title.</returns>
         string GetTitle(CultureInfo culture);
+        /// <summary>
+        /// Get the category of the window.
+        /// </summary>
         SubWindowCategory Category { get; }
     }
-    public enum SubWindowCategory { Overview, Statistics, Information }
+    /// <summary>
+    /// Represent the category of a standalone window.
+    /// </summary>
+    public enum SubWindowCategory
+    {
+        /// <summary>
+        /// Provide overview information of current game.
+        /// </summary>
+        Overview,
+        /// <summary>
+        /// Provide statistic information.
+        /// </summary>
+        Statistics,
+        /// <summary>
+        /// Provide universal information of the game.
+        /// </summary>
+        Information
+    }
 }
