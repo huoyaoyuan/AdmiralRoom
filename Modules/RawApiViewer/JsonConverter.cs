43257220bb3d1b53310c8897be7941e6e31cca6d
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-08-17 20:16:23 +0800

    Json Tree优化

--- Modules/RawApiViewer/JsonConverter.cs
+++ Modules/RawApiViewer/JsonConverter.cs
@@ -12,19 +12,29 @@ namespace RawApiViewer
             switch (parameter.ToString().ToLowerInvariant())
             {
                 case "children":
-                    return ((JToken)value).Children();
-                case "value":
                     if (value is JProperty)
                     {
                         var p = value as JProperty;
+                        return p.Value.Children();
+                    }
+                    else return ((JToken)value).Children();
+                case "value":
+                    JToken v = value as JToken;
+                    if (v is JProperty)
+                    {
+                        var p = v as JProperty;
                         if (p.Value is JContainer) return p.Name;
                         else return p.Name + ":" + p.Value;
                     }
                     else
                     {
-                        if (value is JObject) return "{}";
-                        else if (value is JArray) return "[]";
-                        else return value.ToString();
+                        if (v.Parent is JArray)
+                            if (v is JObject || v is JArray)
+                                return $"[{(v.Parent as JArray).IndexOf(v)}]";
+                            else return $"[{(v.Parent as JArray).IndexOf(v)}]:{v}";
+                        else if (v is JObject) return "{}";
+                        else if (v is JArray) return "[]";
+                        else return v.ToString();
                     }
                 default:
                     throw new ArgumentException(nameof(parameter));
