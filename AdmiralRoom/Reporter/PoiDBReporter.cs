2fec7114418634320ba2eaec1b9d8b1a43b2b95a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-10-09 00:11:42 +0800

    Fix #6: 更新PoiDBReporter的版本号

--- AdmiralRoom/Reporter/PoiDBReporter.cs
+++ AdmiralRoom/Reporter/PoiDBReporter.cs
@@ -9,7 +9,7 @@ namespace Huoyaoyuan.AdmiralRoom.Reporter
     static class PoiDBReporter
     {
         private static readonly string SERVER_HOSTNAME = "poi.0u0.moe";
-        private static readonly string UAString = "AdmiralRoom Reporter v1.0";
+        private static readonly string UAString = "AdmiralRoom Reporter v2";
         public static async void ReportAsync(JObject @object, string apiname)
         {
             if (!Config.Current.ReportToPoiDB) return;
@@ -18,6 +18,7 @@ namespace Huoyaoyuan.AdmiralRoom.Reporter
                 var wrq = WebRequest.CreateHttp($"http://{SERVER_HOSTNAME}/api/report/v2/{apiname}");
                 wrq.UserAgent = UAString;
                 wrq.Method = "POST";
+                @object["origin"] = UAString;
                 var str = "data=" + @object.ToString().Replace("\r\n", "");
                 wrq.ContentType = "text/plain-text";
                 wrq.ContentLength = Encoding.UTF8.GetByteCount(str);
