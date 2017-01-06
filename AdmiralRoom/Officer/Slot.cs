85fa2ea8d5153ba35947b9b149067dbe8c608382
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-01-06 09:25:18 +0800

    喷气机制空计算

--- AdmiralRoom/Officer/Slot.cs
+++ AdmiralRoom/Officer/Slot.cs
@@ -53,7 +53,7 @@ namespace Huoyaoyuan.AdmiralRoom.Officer
 
         public bool HasItem => Item != null;
         public override string ToString() => HasItem && Item.EquipInfo.EquipType.IsAirPlane ? Item + " (" + AirCraft + ")" : Item?.ToString();
-        private static readonly int[] fighttype = { 6, 7, 8, 11, 45 };
+        private static readonly int[] fighttype = { 6, 7, 8, 11, 45, 56, 57, 58 };
         private static readonly int[] bonus1 = { 0, 0, 2, 5, 9, 14, 14, 22 };//舰战，水战
         private static readonly int[] bonus2 = { 0, 0, 1, 1, 1, 3, 3, 6 };//水爆
         private static readonly int[] inner = { 0, 10, 25, 40, 55, 70, 85, 100, 121 };
@@ -66,15 +66,30 @@ namespace Huoyaoyuan.AdmiralRoom.Officer
             get
             {
                 double[] res = new double[8];
-                int itemtype = Item?.EquipInfo.EquipType.Id ?? 0;
-                if (CanProvideAirFightPower)
-                    res[4] = Math.Sqrt(AirCraft.Current) * Item.EquipInfo.AA;
-                else return res;
+                if (Item == null) return res;
                 if (AirCraft.Current == 0) return res;
+                int itemtype = Item.EquipInfo.EquipType.Id;
                 int level = Item.AirProficiency;
+                switch (itemtype)
+                {
+                    case 6://艦上戦闘機
+                    case 45://水上戦闘機
+                    case 56://噴式戦闘機
+                        res[6] = res[7] = bonus1[level];
+                        break;
+                    case 11://水上爆撃機
+                        res[6] = res[7] = bonus2[level];
+                        break;
+                    case 7://艦上爆撃機
+                    case 8://艦上攻撃機
+                    case 57://噴式戦闘爆撃機
+                    case 58://噴式攻撃機
+                        break;
+                    default:
+                        return res;
+                }
+                res[4] = Math.Sqrt(AirCraft.Current) * Item.EquipInfo.AA;
                 res[5] = itemtype == 6 ? res[4] : 0;
-                if (itemtype == 6 || itemtype == 45) res[6] = res[7] = bonus1[level];
-                else if (itemtype == 11) res[6] = res[7] = bonus2[level];
                 res[6] += Math.Sqrt(inner[level] / 10.0) + Math.Sqrt(AirCraft.Current) * Item.ImproveLevel * 0.2;
                 res[7] += Math.Sqrt((inner[level + 1] - 1) / 10.0) + Math.Sqrt(AirCraft.Current) * Item.ImproveLevel * 0.2;
                 res[3] = itemtype == 6 || itemtype == 45 ? res[4] + res[7] : 0;
